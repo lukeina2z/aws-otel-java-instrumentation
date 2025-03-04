7b780e94dba26189185d7a803d9187234edb65cf
Luke Zhang <lukezha@amazon.com>
2025-03-04 12:20:20 -0800

    xfix: update build script, clone db, no patching.

--- lambda-layer/build-layer.sh
+++ lambda-layer/build-layer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 SOURCEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 
@@ -15,34 +15,37 @@ fi
 
 
 ## Clone and Patch the OpenTelemetry Java Instrumentation Repository
-git clone https://github.com/open-telemetry/opentelemetry-java-instrumentation.git
+# git clone https://github.com/open-telemetry/opentelemetry-java-instrumentation.git
+git clone git@github.com:lukeina2z/opentelemetry-java-instrumentation.git
 pushd opentelemetry-java-instrumentation
-git checkout v${version} -b tag-v${version}
-
-# This patch is for Lambda related context propagation
-patch -p1 < "$SOURCEDIR"/patches/opentelemetry-java-instrumentation.patch
-
-# There is another patch in the .github/patches directory for other changes. We should apply them too for consistency.
-patch -p1 < "$SOURCEDIR"/../.github/patches/opentelemetry-java-instrumentation.patch
-
-git add -A
-git commit -m "Create patch version"
-./gradlew publishToMavenLocal
-popd
-rm -rf opentelemetry-java-instrumentation
+# git checkout v${version} -b tag-v${version}
+git checkout origin/lk-v${version}-fix -b tag-v${version}
 
 
-## Build the ADOT Java from current source
-pushd "$SOURCEDIR"/..
-patch  -p1 < "${SOURCEDIR}"/patches/aws-otel-java-instrumentation.patch
-CI=false ./gradlew publishToMavenLocal -Prelease.version=${version}-adot-lambda1
-popd
+# # This patch is for Lambda related context propagation
+# patch -p1 < "$SOURCEDIR"/patches/opentelemetry-java-instrumentation.patch
+
+# # There is another patch in the .github/patches directory for other changes. We should apply them too for consistency.
+# patch -p1 < "$SOURCEDIR"/../.github/patches/opentelemetry-java-instrumentation.patch
+
+# git add -A
+# git commit -m "Create patch version"
+# ./gradlew publishToMavenLocal
+# popd
+# rm -rf opentelemetry-java-instrumentation
 
 
-## Build ADOT Lambda Java SDK Layer Code
-./gradlew build
+# ## Build the ADOT Java from current source
+# pushd "$SOURCEDIR"/..
+# patch  -p1 < "${SOURCEDIR}"/patches/aws-otel-java-instrumentation.patch
+# CI=false ./gradlew publishToMavenLocal -Prelease.version=${version}-adot-lambda1
+# popd
 
 
-## Copy ADOT Java Agent downloaded using Gradle task and bundle it with the Lambda handler script
-cp "$SOURCEDIR"/build/javaagent/aws-opentelemetry-agent*.jar ./opentelemetry-javaagent.jar
-zip -qr opentelemetry-javaagent-layer.zip opentelemetry-javaagent.jar otel-instrument
+# ## Build ADOT Lambda Java SDK Layer Code
+# ./gradlew build
+
+
+# ## Copy ADOT Java Agent downloaded using Gradle task and bundle it with the Lambda handler script
+# cp "$SOURCEDIR"/build/javaagent/aws-opentelemetry-agent*.jar ./opentelemetry-javaagent.jar
+# zip -qr opentelemetry-javaagent-layer.zip opentelemetry-javaagent.jar otel-instrument
