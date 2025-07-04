a034a46eff2d2ad2a4ae47980f58fd4e4fe51324
Luke Zhang <lukezha@amazon.com>
2025-07-04 11:16:49 -0700

    xyz-99: Lambda Java layer starts using 99 build of ADOT & OTel Java Agent.

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -27,7 +27,7 @@ data class DependencySet(val group: String, val version: String, val modules: Li
 val testSnapshots = rootProject.findProperty("testUpstreamSnapshots") == "true"
 
 // This is the version of the upstream instrumentation BOM
-val otelVersion = "2.11.0-adot1"
+val otelVersion = "2.11.99-adot1"
 val otelSnapshotVersion = "2.12.0"
 val otelAlphaVersion = if (!testSnapshots) "$otelVersion-alpha" else "$otelSnapshotVersion-alpha-SNAPSHOT"
 val otelJavaAgentVersion = if (!testSnapshots) otelVersion else "$otelSnapshotVersion-SNAPSHOT"
@@ -73,8 +73,8 @@ val dependencyLists = listOf(
   "commons-logging:commons-logging:1.2",
   "com.sparkjava:spark-core:2.9.4",
   "com.squareup.okhttp3:okhttp:4.12.0",
-  "io.opentelemetry.contrib:opentelemetry-aws-xray:1.39.0-adot1",
-  "io.opentelemetry.contrib:opentelemetry-aws-resources:1.39.0-alpha",
+  "io.opentelemetry.contrib:opentelemetry-aws-xray:1.39.99-adot1",
+  "io.opentelemetry.contrib:opentelemetry-aws-resources:1.39.99-alpha-adot1",
   "io.opentelemetry.proto:opentelemetry-proto:1.0.0-alpha",
   "io.opentelemetry.javaagent:opentelemetry-javaagent:$otelJavaAgentVersion",
   "io.opentelemetry:opentelemetry-extension-aws:1.20.1",
